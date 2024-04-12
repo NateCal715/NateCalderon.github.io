5347329df7390e0c3d9f9b2f219aaafc8d0bafc4
NateCal715 <157073389+NateCal715@users.noreply.github.com>
2024-04-12 20:00:12 +0000

    modified:   dist/bundle.js

--- dist/bundle.js
+++ dist/bundle.js
@@ -7,26 +7,43 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("\n\n//# sourceURL=webpack://natecalderon.github.io/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\nObject(function webpackMissingModule() { var e = new Error(\"Cannot find module 'firebase/app'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\nObject(function webpackMissingModule() { var e = new Error(\"Cannot find module 'firebase/auth'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\n\n\n\n\n// For Firebase JS SDK v7.20.0 and later, measurementId is optional\nconst firebaseConfig = {\n    apiKey: \"AIzaSyB_vZta6FIMoSPYkqhS2ljRjjEjAiQ6qxg\",\n    authDomain: \"health-tracker-dbc6d.firebaseapp.com\",\n    projectId: \"health-tracker-dbc6d\",\n    storageBucket: \"health-tracker-dbc6d.appspot.com\",\n    messagingSenderId: \"164968132870\",\n    appId: \"1:164968132870:web:68aa1101d18ca26deeae87\",\n    measurementId: \"G-W4LRQT3X59\"\n  };\n  \n  //init firebase app\n  Object(function webpackMissingModule() { var e = new Error(\"Cannot find module 'firebase/app'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }())(firebaseConfig);\n\n// init firebase services\nconst auth = Object(function webpackMissingModule() { var e = new Error(\"Cannot find module 'firebase/auth'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }())()\n\n\n// Signing Users up with Email and Password\nconst signupForm = document.querySelector('.signup')\nsignupForm.addEventListener('submit', (e) => {\n  e.preventDefault()\n\n  const email = signupForm.email.value\n  const password = signupForm.password.value\n\n  Object(function webpackMissingModule() { var e = new Error(\"Cannot find module 'firebase/auth'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }())(auth, email, password)\n    .then((cred) => {\n      console.log('user created:', cred.user)\n      signupForm.reset()\n    })\n    .catch((err) => {\n      console.log(err.message)\n    })\n})\n\n//# sourceURL=webpack://natecalderon.github.io/./src/index.js?");
 
 /***/ })
 
 /******/ 	});
+/************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	__webpack_modules__["./src/index.js"](0, __webpack_exports__, __webpack_require__);
 /******/ 	
 /******/ })()
 ;
