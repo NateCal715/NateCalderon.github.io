90eb60ee516fe91a5272d2f0a48be7652a4821c6
NateCal715 <157073389+NateCal715@users.noreply.github.com>
2024-04-15 15:39:22 +0000

    modified:   dist/bundle.js 	modified:   login.html 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -41,3 +41,31 @@ signupForm.addEventListener('submit', (e) => {
       console.log(err.message)
     })
 })
+
+//Logging Users in and out
+const logoutButton = document.querySelector('.logout')
+logoutButton.addEventListener('click', () => {
+  signOut(auth)
+    .then(() => {
+      console.log('the user signed out')
+    })
+    .catch((err) => {
+      console.log(err.message)
+    })
+})
+
+const loginForm = document.querySelector('.login')
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const email = loginForm.email.value
+  const password = loginForm.password.value
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((cred) => {
+      console.log('user logged in:' cred.user)
+    })
+    .catch((err) => {
+      console.log(err.message)
+    })
+})
